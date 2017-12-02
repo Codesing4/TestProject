ab0419a94c4075ff8f6a6dc13e47f7a12a7db3ac
iAbdu <fars_10@hotmail.com>
2017-12-01 19:53:19 -0500

    this is the project file with app icons.

--- TestProject/ViewController.swift
+++ TestProject/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world")
-        
+    
         print("Another Hello world")
     }
 
